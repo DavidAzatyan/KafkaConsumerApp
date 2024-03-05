c2c5b1cd3e0d98767c423c295211d29143b12841
DavidAzatyan <azatyand27@gmail.com>
2024-03-05 04:16:54 +0400

    Change class name.

--- src/main/java/org/spring/kafka/KafkaConsumerApp.java
+++ src/main/java/org/spring/kafka/KafkaConsumerApp.java
@@ -6,9 +6,7 @@ import org.spring.kafka.consumer.KafkaMessageConsumer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-
-
-public class MainApp {
+public class KafkaConsumerApp {
     public static void main(String[] args) {
         ApplicationContext context = new AnnotationConfigApplicationContext(
                 KafkaConsumerConfig.class, DataSourceConfig.class, KafkaMessageConsumer.class);
